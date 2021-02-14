f425e0432546f9baf44daef88aa7e3670fd6f334
unknown <juliagiorgimartin@hotmail.com>
2021-02-14 15:33:53 -0300

    Criando usuários

---
+++ backend/src/database/migrations/1613241182631-CreateAppointments.ts
@@ -0,0 +1,61 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateAppointments1613241182631
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider_id',
+            type: 'varchar',
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+    await queryRunner.dropTable('appointments');
+  }
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import usersRouter from './users.route';
 import appointmentsRouter from './appointmens.routes';
 
 const routes = Router();
+
+routes.use('/users', usersRouter);
 
 routes.use('/appointments', appointmentsRouter);
 
