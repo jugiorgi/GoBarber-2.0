9e163f61e8246b96a416e7ee6036823b07d6fdcb
unknown <juliagiorgimartin@hotmail.com>
2021-02-14 18:57:24 -0300

    Iniciando aplicação web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
