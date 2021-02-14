1e1a32c5267ce2c3107a39c9dcadc8666b7c85bc
unknown <juliagiorgimartin@hotmail.com>
2021-02-14 16:51:15 -0300

    Rotas autenticadas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.route';
 import appointmentsRouter from './appointmens.routes';
 
 const routes = Router();
+
+routes.use('/sessions', sessionsRouter);
 
 routes.use('/users', usersRouter);
 
